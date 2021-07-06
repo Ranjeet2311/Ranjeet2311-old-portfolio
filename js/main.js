4a79596d94293f62ac685097ffc3f0acde3353fc
Ranjeet Kumar <ranjeetkumar2311@gmail.com>
2021-07-06 07:47:43 +0200

    firstCommit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-const nav = document.querySelector('.nav-menu');
-const Git = document.querySelector('.Git');
-const burger = document.querySelector('.burger')
+const nav = document.querySelector(".nav-menu");
+const Git = document.querySelector(".Git");
+const burger = document.querySelector(".burger");
+const avatarContainer = document.querySelector(".Avatar-Home");
+const avatarHi = document.querySelector(".Avatar-Hi");
+const avatarNamaste = document.querySelector(".Avatar-Namaste");
 
-burger.addEventListener('click', () =>{
-      nav.classList.toggle('V-opacity');
-      nav.classList.toggle('N-height');
-      Git.classList.toggle('V-opacity');
-})
+burger.addEventListener("click", () => {
+  nav.classList.toggle("V-opacity");
+  nav.classList.toggle("N-height");
+  Git.classList.toggle("V-opacity");
+});
+
+avatarContainer.addEventListener("mouseover", () => {
+  avatarHi.classList.remove("Avtar_Hide");
+  avatarNamaste.classList.add("Avtar_Hide");
+});
+avatarContainer.addEventListener("mouseout", () => {
+  avatarHi.classList.add("Avtar_Hide");
+  avatarNamaste.classList.remove("Avtar_Hide");
+});
