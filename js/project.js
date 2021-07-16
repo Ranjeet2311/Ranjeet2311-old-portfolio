b54d186634d081c3752e30ee781527e0e46c87af
Ranjeet Kumar <ranjeetkumar2311@gmail.com>
2021-07-16 19:28:02 +0200

    Website Temp displayed

--- js/project.js
+++ js/project.js
@@ -1,4 +1,4 @@
-const projectList = [
+const webAppProjectList = [
   {
     id: 1,
     img: "/app-banner/to-do-tasks.jpg",
@@ -49,6 +49,57 @@ const projectList = [
     demo: "/app-demo/counter.html",
   },
 ];
+const webTemplatesProjectList = [
+  {
+    id: 1,
+    img: "/app-banner/e-commerce-demo.png",
+    title: "e-commerce",
+    code: "http://google.com/",
+    demo: "/app-demo/e-commerce-demo.html",
+  },
+  {
+    id: 2,
+    img: "/app-banner/weight-converter.jpg",
+    title: "Website 2",
+    code: "http://google.com/",
+    demo: "/app-demo/weight-converter.html",
+  },
+  {
+    id: 3,
+    img: "/app-banner/Digital-Clock.jpg",
+    title: "Website 3",
+    code: "http://google.com/",
+    demo: "/app-demo/digital-clock.html",
+  },
+  {
+    id: 4,
+    img: "/app-banner/modal.jpg",
+    title: "Website 4",
+    code: "http://google.com/",
+    demo: "/app-demo/modal.html",
+  },
+  {
+    id: 5,
+    img: "/app-banner/play-music.jpg",
+    title: "Website 5",
+    code: "http://google.com/",
+    demo: "/app-demo/play-music.html",
+  },
+  // {
+  //   id: 6,
+  //   img: "/app-banner/music-player.jpg",
+  //   title: "Music player",
+  //   code: "http://google.com/",
+  //   demo: "/app-demo/music-player.html",
+  // },
+  // {
+  //   id: 7,
+  //   img: "/app-banner/counter.jpg",
+  //   title: "Counter",
+  //   code: "http://google.com/",
+  //   demo: "/app-demo/counter.html",
+  // },
+];
 
 const nav = document.querySelector(".nav-menu");
 const Git = document.querySelector(".Git");
@@ -57,6 +108,9 @@ const avatarContainer = document.querySelector(".Avatar-Home");
 const avatarHi = document.querySelector(".Avatar-Hi");
 const avatarNamaste = document.querySelector(".Avatar-Namaste");
 let webAppProjectLinks = document.querySelector(".web-app-project-links");
+let webTemplateProjectLinks = document.querySelector(
+  ".web-template-project-links"
+);
 
 burger.addEventListener("click", () => {
   nav.classList.toggle("V-opacity");
@@ -73,9 +127,9 @@ burger.addEventListener("click", () => {
 //   avatarHi.classList.add("Avtar_Hide");
 //   avatarNamaste.classList.remove("Avtar_Hide");
 // });
-// ---------Project mapping ---
+// ---------Web App Project mapping ---
 
-let projectDisplay = projectList.map(function (item) {
+let projectDisplay = webAppProjectList.map(function (item) {
   return `                  <div class="links-container">
         <img src= ${item.img} >
         <h4>${item.title}</h4>
@@ -94,4 +148,24 @@ let projectDisplay = projectList.map(function (item) {
 projectDisplay = projectDisplay.join("");
 webAppProjectLinks.innerHTML = projectDisplay;
 
-// console.log(projectDisplay);
+// ---------Web Template Project mapping ------
+let webTemplateProjectDisplay = webTemplatesProjectList.map(function (item) {
+  return `                  <div class="links-container">
+        <img src= ${item.img} >
+        <h4>${item.title}</h4>
+        <div class="links-container-btn">
+              <button class="btn-links btn"> <a href=${item.code}> <i class="fas fa-download"></i> source code
+                    </a></button>
+              <button class="btn-links btn"> <a
+                          href=${item.demo}
+                          target=”_blank”>Live
+                          Demo</a>
+              </button>
+        </div>
+    
+      </div> `;
+});
+webTemplateProjectDisplay = webTemplateProjectDisplay.join("");
+webTemplateProjectLinks.innerHTML = webTemplateProjectDisplay;
+
+console.log(webTemplateProjectDisplay);
