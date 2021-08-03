2baecb84ab12409705b9177adb79b8ec3ef97a14
Ranjeet Kumar <ranjeetkumar2311@gmail.com>
2021-08-03 11:48:26 +0200

    react-live projects added & CV Updated

--- js/project.js
+++ js/project.js
@@ -126,6 +126,8 @@ let webAppProjectLinks = document.querySelector(".web-app-project-links");
 let webTemplateProjectLinks = document.querySelector(
   ".web-template-project-links"
 );
+let reactAppProjectLinks = document.querySelector(".react-app-project-links");
+
 const faDotCircle = document.querySelector(".fa-dot-circle");
 faDotCircle.classList.remove("blink");
 
@@ -178,122 +180,184 @@ webTemplateProjectLinks.innerHTML = webTemplateProjectDisplay;
 // console.log(webTemplateProjectDisplay);
 
 // -----------------React project Display---------------
-
-const reactImages = [
+const reactProjectList = [
   {
     id: 1,
-    img: "/reactScreenshots/React-web/React-home.png",
-    text: "React-Home",
+    img: "/reactScreenshots/movie-database-project.png",
+    title: "Movie Database",
+    code: "https://github.com/Ranjeet2311/React-MovieDatabase-app",
+    demo: "https://movie-database-project-0802.netlify.app/",
   },
   {
     id: 2,
-    img: "/reactScreenshots/React-web/React-services.png",
-    text: "React-services",
+    img: "/reactScreenshots/React-home.png",
+    title: "React-Website",
+    code: "https://github.com/Ranjeet2311/React-Website",
+    demo: "https://react-website-0803.netlify.app/",
   },
   {
     id: 3,
-    img: "/reactScreenshots/React-web/React-contact.png",
-    text: "React-contact",
-  },
-  {
-    id: 4,
-    img: "/reactScreenshots/React-web/React-about-us.png",
-    text: "React-about-us",
-  },
-  {
-    id: 5,
-    img: "/reactScreenshots/React-web/React-smartphone-burger.png",
-    text: "React-smartphone-burger",
-  },
-  {
-    id: 6,
-    img: "/reactScreenshots/React-web/React-smartphone-close.png",
-    text: "React-smartphone-close",
+    img: "/reactScreenshots/react-menu-home.png",
+    title: "React Dynamic Menu",
+    code: "https://github.com/Ranjeet2311/React-Navbar",
+    demo: "https://react-navigation-menu-0803.netlify.app/",
   },
 ];
 
+let reactProjectDisplay = reactProjectList.map(function (item) {
+  return `<div class="links-container">
+  <img src= ${item.img} >
+  <h4>${item.title}</h4>
+  <div class="links-container-btn">
+        <button class="btn-links btn"> <a href=${item.code}> <i class="fas fa-download"></i> source code
+              </a></button>
+        <button class="btn-links btn"> <a
+                    href=${item.demo}
+                    target=”_blank”>Live
+                    Demo</a>
+        </button>
+  </div>
+
+</div>`;
+});
+reactProjectDisplay = reactProjectDisplay.join(" ");
+reactAppProjectLinks.innerHTML = reactProjectDisplay;
+
+// let reactAppProjectLinks = reactProjectList.map(function (item) {
+//   return `                  <div class="links-container">
+//         <img src= ${item.img} >
+//         <h4>${item.title}</h4>
+//         <div class="links-container-btn">
+//               <button class="btn-links btn"> <a href=${item.code}> <i class="fas fa-download"></i> source code
+//                     </a></button>
+//               <button class="btn-links btn"> <a
+//                           href=${item.demo}
+//                           target=”_blank”>Live
+//                           Demo</a>
+//               </button>
+//         </div>
+
+//       </div> `;
+// });
+// reactAppProjectLinks = reactAppProjectLinks.join("");
+// reactAppProjectLinks.innerHTML = reactAppProjectLinks;
+
+// const reactImages = [
+//   {
+//     id: 1,
+//     img: "/reactScreenshots/React-web/React-home.png",
+//     text: "React-Home",
+//   },
+//   {
+//     id: 2,
+//     img: "/reactScreenshots/React-web/React-services.png",
+//     text: "React-services",
+//   },
+//   {
+//     id: 3,
+//     img: "/reactScreenshots/React-web/React-contact.png",
+//     text: "React-contact",
+//   },
+//   {
+//     id: 4,
+//     img: "/reactScreenshots/React-web/React-about-us.png",
+//     text: "React-about-us",
+//   },
+//   {
+//     id: 5,
+//     img: "/reactScreenshots/React-web/React-smartphone-burger.png",
+//     text: "React-smartphone-burger",
+//   },
+//   {
+//     id: 6,
+//     img: "/reactScreenshots/React-web/React-smartphone-close.png",
+//     text: "React-smartphone-close",
+//   },
+// ];
+
 // ------------React --website --
-const image = document.querySelector("#image");
-const reactTitle = document.querySelector("#reactTitle");
-const prevBtn = document.querySelector(".prev-btn");
-const nextBtn = document.querySelector(".next-btn");
 
-let currentItem = 0;
+// const image = document.querySelector("#image");
+// const reactTitle = document.querySelector("#reactTitle");
+// const prevBtn = document.querySelector(".prev-btn");
+// const nextBtn = document.querySelector(".next-btn");
 
-image.src = reactImages[currentItem].img;
-reactTitle.textContent = reactImages[currentItem].text;
+// let currentItem = 0;
 
-function slideShow() {
-  reactImages[currentItem];
-  image.src = reactImages[currentItem].img;
-  reactTitle.textContent = reactImages[currentItem].text;
-}
+// image.src = reactImages[currentItem].img;
+// reactTitle.textContent = reactImages[currentItem].text;
 
-nextBtn.addEventListener("click", () => {
-  currentItem++;
-  if (currentItem >= reactImages.length) {
-    currentItem = 0;
-  }
-  slideShow();
-});
+// function slideShow() {
+//   reactImages[currentItem];
+//   image.src = reactImages[currentItem].img;
+//   reactTitle.textContent = reactImages[currentItem].text;
+// }
 
-prevBtn.addEventListener("click", () => {
-  currentItem--;
-  if (currentItem < 0) {
-    currentItem = reactImages.length - 1;
-  }
-  slideShow();
-});
+// nextBtn.addEventListener("click", () => {
+//   currentItem++;
+//   if (currentItem >= reactImages.length) {
+//     currentItem = 0;
+//   }
+//   slideShow();
+// });
+
+// prevBtn.addEventListener("click", () => {
+//   currentItem--;
+//   if (currentItem < 0) {
+//     currentItem = reactImages.length - 1;
+//   }
+//   slideShow();
+// });
 
 // -----------------------------React-menu ---------------------
 
-const menuImages = [
-  {
-    id: 1,
-    pic: "/reactScreenshots/React-menu/react-menu-main-home.png",
-    text: "Menu-home",
-  },
-  {
-    id: 2,
-    pic: "/reactScreenshots/React-menu/react-menu-home.png",
-    text: "Smart device burger",
-  },
-  {
-    id: 3,
-    pic: "/reactScreenshots/React-menu/react-menu-smart.png",
-    text: "Menu Display",
-  },
-];
+// const menuImages = [
+//   {
+//     id: 1,
+//     pic: "/reactScreenshots/React-menu/react-menu-main-home.png",
+//     text: "Menu-home",
+//   },
+//   {
+//     id: 2,
+//     pic: "/reactScreenshots/React-menu/react-menu-home.png",
+//     text: "Smart device burger",
+//   },
+//   {
+//     id: 3,
+//     pic: "/reactScreenshots/React-menu/react-menu-smart.png",
+//     text: "Menu Display",
+//   },
+// ];
 
-const menuImage = document.querySelector(".image");
-const menuTitle = document.querySelector(".reactTitle");
-const menuPrevBtn = document.querySelector(".menu-prev-btn");
-const menuNextBtn = document.querySelector(".menu-next-btn");
+// const menuImage = document.querySelector(".image");
+// const menuTitle = document.querySelector(".reactTitle");
+// const menuPrevBtn = document.querySelector(".menu-prev-btn");
+// const menuNextBtn = document.querySelector(".menu-next-btn");
 
-let currentImg = 0;
+// let currentImg = 0;
 
-menuImage.src = menuImages[currentImg].pic;
-menuTitle.textContent = menuImages[currentImg].text;
+// menuImage.src = menuImages[currentImg].pic;
+// menuTitle.textContent = menuImages[currentImg].text;
 
-function menuSlideShow() {
-  menuImages[currentImg];
-  menuImage.src = menuImages[currentImg].pic;
-  menuTitle.textContent = menuImages[currentImg].text;
-}
+// function menuSlideShow() {
+//   menuImages[currentImg];
+//   menuImage.src = menuImages[currentImg].pic;
+//   menuTitle.textContent = menuImages[currentImg].text;
+// }
 
-menuPrevBtn.addEventListener("click", function () {
-  currentImg--;
-  if (currentImg < 0) {
-    currentImg = menuImages.length - 1;
-  }
-  menuSlideShow();
-  console.log(123);
-});
-menuNextBtn.addEventListener("click", function () {
-  currentImg++;
-  if (currentImg >= menuImages.length) {
-    currentImg = 0;
-  }
-  menuSlideShow();
-  console.log(123);
-});
+// menuPrevBtn.addEventListener("click", function () {
+//   currentImg--;
+//   if (currentImg < 0) {
+//     currentImg = menuImages.length - 1;
+//   }
+//   menuSlideShow();
+//   console.log(123);
+// });
+// menuNextBtn.addEventListener("click", function () {
+//   currentImg++;
+//   if (currentImg >= menuImages.length) {
+//     currentImg = 0;
+//   }
+//   menuSlideShow();
+//   console.log(123);
+// });
